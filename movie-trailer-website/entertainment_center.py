b1722a019b7873457de5430b49afd26dfa862815
Aditya <me@adityamhamunkar.com>
2017-06-30 11:23:43 -0400

    Updated for resubmit

--- movie-trailer-website/entertainment_center.py
+++ movie-trailer-website/entertainment_center.py
@@ -1,38 +1,49 @@
 import fresh_tomatoes
 import media
-# creating objects of class Movie
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-avatar = media.Movie("Avatar",
-                  "A story of marine",
-                  "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                  "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+# Toy story movie: movie title, storyline, poster image, and trailer
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys that come to life",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
+# Avatar movie: movie title, storyline, poster image, and trailer
+avatar = media.Movie(
+    "Avatar",
+    "A story of marine",
+    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
-lor = media.Movie("Lord of the Rings",
-                  "A story of a powerful ring",
-                  "https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg",
-                  "https://www.youtube.com/watch?v=Pki6jbSbXIY")
+# Lords of the rings movie: movie title, storyline, poster image, and trailer
+lor = media.Movie(
+    "Lord of the Rings",
+    "A story of a powerful ring",
+    "https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=Pki6jbSbXIY")
 
-forest = media.Movie("Forest Gump",
-                  "A story of a different guy!",
-                  "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
-                  "https://www.youtube.com/watch?v=bLvqoHBptjg")
+# Forest Gump movie: movie title, storyline, poster image, and trailer
+forest = media.Movie(
+    "Forest Gump",
+    "A story of a different guy!",
+    "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
-happiness = media.Movie("Pursuit of happyness",
-                  "An inspiring story",
-                  "https://upload.wikimedia.org/wikipedia/en/8/81/Poster-pursuithappyness.jpg",
-                  "https://www.youtube.com/watch?v=bTJ-0zVAsEA")
+# Pursuit of happyness movie: movie title, storyline, poster image, and trailer
+happiness = media.Movie(
+    "Pursuit of happyness",
+    "An inspiring story",
+    "https://upload.wikimedia.org/wikipedia/en/8/81/Poster-pursuithappyness.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=bTJ-0zVAsEA")
 
-swadees = media.Movie("Swadees",
-                  "A story of a true Indian",
-                  "https://upload.wikimedia.org/wikipedia/en/8/85/Swades_poster.jpg",
-                  "https://www.youtube.com/watch?v=x6QOtayfj-0")
+# Swades movie: movie title, storyline, poster image, and trailer
+swadees = media.Movie(
+    "Swadees",
+    "A story of a true Indian",
+    "https://upload.wikimedia.org/wikipedia/en/8/85/Swades_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=x6QOtayfj-0")
 
-#List of Movie objects
-movies = [toy_story, avatar, lor, forest,happiness,swadees]
+# List of Movie objects
+movies = [toy_story, avatar, lor, forest, happiness, swadees]
 
-#calling open_movies_page() from fresh_tomatoes module/file
+# calling open_movies_page() from fresh_tomatoes module/file
 fresh_tomatoes.open_movies_page(movies)
--- movie-trailer-website/media.py
+++ movie-trailer-website/media.py
@@ -1,18 +1,19 @@
 import webbrowser
 
-#super class Video
+''' super class Video '''
 class Video():
     def __init__(self, title):
         self.title = title
 
 
-#class Movie which is inherited from class Video        
+''' class Movie which is inherited from class Video '''
 class Movie(Video):
-    def __init__(self, title, storyline,poster,trailer):
+    def __init__(self, title, storyline, poster, trailer):
         Video.__init__(self, title)
         self.storyline = storyline
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
 
+    ''' this method opens the browser and displays the trailer '''
     def show_trailer(self):
         webbrowser.open(self.trailer)
